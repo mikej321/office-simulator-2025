18aace313015f619ef553a1b2417462ce1e943ba
Michael Johnson <michaeljcantley@gmail.com>
2025-02-12 16:05:55 -0500

    Fix font issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/office-simulator-2025/",
+  build: {
+    outDir: "../docs",
+  },
   server: {
     proxy: {
       "/api": "http://localhost:5000",
