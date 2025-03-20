8cfcf394316c1a4923d9e9e220575bf5bacb6031
Bryce Freshwater <bfreshwater@gmail.com>
2025-03-20 18:14:58 -0400

    added mini game and stats manager

--- frontend/public/assets/asset-export-final-resized.tsx
+++ frontend/public/assets/asset-export-final-resized.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="asset-export-final-resized" tilewidth="16" tileheight="16" tilecount="832" columns="32">
  <image source="asset-export-final-resized.png" width="512" height="416"/>
+ <tile id="306">
+  <properties>
+   <property name="interactable: true" value=""/>
+  </properties>
+ </tile>
 </tileset>
