e7dc46bd13ae850bf99f11fa1b27b7f89643111c
Bryce Freshwater <bfreshwater@gmail.com>
2025-06-13 09:02:09 -0400

    quick changes

--- frontend/src/phaser/scenes/HomeEvening.js
+++ frontend/src/phaser/scenes/HomeEvening.js
@@ -134,7 +134,7 @@ class HomeEvening extends Phaser.Scene {
   }
 
   createPlayer() {
-    this.player = this.physics.add.sprite(900, 600, "player", "frame-1").setScale(0.8);
+    this.player = this.physics.add.sprite(800, 300, "player", "frame-1").setScale(0.8);
     this.player.setSize(32, 32);
 
     if (!this.anims.exists("walk")) {
